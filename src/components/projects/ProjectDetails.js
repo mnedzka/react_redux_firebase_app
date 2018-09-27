afc0c4d32961ef87f55bcf14f99d1c9cceda3f86
Michal Nedzka <michael.nedzka@gmail.com>
2018-09-27 23:06:54 +0200

    Add project details component and align routing

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ProjectDetails = props => {
+  const id = rops.match.params.id;
+  return (
+    <div className="container setcion project-details">
+      <div className="card z-depth-zero">
+        <div className="card-content">
+          <span className="card-title">
+            Project title
+            {id}
+          </span>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Exercitationem, cupiditate doloremque mollitia accusantium ad iste
+            voluptas eum quibusdam animi, ab incidunt. Sequi id magni
+            dignissimos minima aspernatur, minus reiciendis? Iure.
+          </p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>posted by the net ninjs</div>
+          <div>date</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
