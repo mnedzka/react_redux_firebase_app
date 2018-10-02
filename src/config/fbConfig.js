5982267bb1a9ef84b5f1e602eb4fa2545e085a3f
Michal Nedzka <michael.nedzka@gmail.com>
2018-10-02 21:19:07 +0200

    Add Firebase config

---
+++ src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import { firebase } from "firbease/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyA9F0wu-BLN1wpURgxGa0xzhPt5H-012gM",
+  authDomain: "net-ninja-marioplan-4c3bb.firebaseapp.com",
+  databaseURL: "https://net-ninja-marioplan-4c3bb.firebaseio.com",
+  projectId: "net-ninja-marioplan-4c3bb",
+  storageBucket: "net-ninja-marioplan-4c3bb.appspot.com",
+  messagingSenderId: "206802516809"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
