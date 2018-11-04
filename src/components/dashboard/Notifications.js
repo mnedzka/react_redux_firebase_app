77210814f70f1ed219faafb03e8f5fd31ccb490d
Michal Nedzka <michael.nedzka@gmail.com>
2018-11-04 22:55:59 +0100

    Add dynamic notifications and connect them to firebase

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,16 +1,26 @@
 import React from "react";
+import moment from "moment";
 
 const Notifications = props => {
+  const { notifications } = props;
   return (
     <div className="section">
       <div className="card z-depth-0">
         <div className="card-content">
           <div className="card-title">Notifications</div>
           <ul className="notification">
-            <li>Notification</li>
-            <li>Notification</li>
-            <li>Notification</li>
-            <li>Notification</li>
+            {notifications &&
+              notifications.map(item => {
+                return (
+                  <li key={item.id}>
+                    <span className="pink-text">{item.user}</span>
+                    <span>{item.content}</span>
+                    <div className="grey-text note-date">
+                      {moment(item.time.toDate()).fromNow}
+                    </div>
+                  </li>
+                );
+              })}
           </ul>
         </div>
       </div>
